e02cbafa0e34a107e46ade37218a2eb54dce6102
Abdulahi Said <zkac230@live.rhul.ac.uk>
2025-03-10 12:05:54 +0000

    refac: rf_model now uses joblib parallelism to train multiple trees in parallel.

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,6 +1,7 @@
 from models.decision_tree import DecisionTree
 import numpy as np
 from collections import Counter
+from joblib import Parallel, delayed  # Import parallel processing utilities
 
 class RandomForest:
     def __init__(self, n_estimators=10, max_depth=10, min_samples_split=2, n_features=None, uniformity_measure="gini"):
@@ -29,17 +30,34 @@ class RandomForest:
             X (numpy array): Training data features.
             y (numpy array): Training data labels.
         """
-        self.trees = []
-        for _ in range(self.n_estimators):
-            tree = DecisionTree(
-                uniformity_measure=self.uniformity_measure,
-                max_depth=self.max_depth,
-                min_samples_split=self.min_samples_split,
-                n_features=self.n_features
-            )
-            X_sample, y_sample = self.select_bootstrap_sample(X, y) # take a random sample of the data
-            tree.fit(X_sample, y_sample)
-            self.trees.append(tree)
+        # Parallelize the tree training
+        # # n_jobs=-1 uses all available CPU cores
+        self.trees = Parallel(n_jobs=-1)(  
+            delayed(self.train_tree)(X, y) for _ in range(self.n_estimators)
+        )
+
+    def train_tree(self, X, y):
+        """
+        Helper function to train a single tree on a bootstrap sample.
+        Will be parallelised by joblib.
+        
+        Parameters:
+            X (numpy array): Training data features.
+            y (numpy array): Training data labels.
+            
+        Returns:
+            tree (DecisionTree): A trained decision tree.
+        
+        """
+        tree = DecisionTree(
+            uniformity_measure=self.uniformity_measure,
+            max_depth=self.max_depth,
+            min_samples_split=self.min_samples_split,
+            n_features=self.n_features
+        )
+        X_sample, y_sample = self.select_bootstrap_sample(X, y)  # Bootstrap sampling
+        tree.fit(X_sample, y_sample)  # Fit the tree on the bootstrap sample
+        return tree
 
     def select_bootstrap_sample(self, X, y):
         """
@@ -74,8 +92,7 @@ class RandomForest:
         counter = Counter(preds)
         most_common_label = counter.most_common(1)[0][0]
         return most_common_label
-    
-    
+
     def predict(self, X):
         """
         Predict the class labels for the input samples X.
@@ -86,11 +103,12 @@ class RandomForest:
         Returns:
             predictions (numpy array): Predicted class labels.
         """
+        # Collect predictions from each tree
         tree_predictions = np.array([tree.predict(X) for tree in self.trees])
         tree_predictions = np.swapaxes(tree_predictions, 0, 1)
+        # Majority vote for final prediction
         return np.array([self.majority_vote(preds) for preds in tree_predictions])
-    
-    
+
     def clone(self):
         """
         Create a new instance of RandomForest with the same parameters.
